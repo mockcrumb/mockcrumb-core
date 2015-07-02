430a8d9ea7fbb228a519335822f680dae5152831
Maciej Gowin <maciej.abacus.gowin@gmail.com>
2015-07-02 21:45:45 +0100

    Extended sample applications

--- src/main/java/org/mockcrumb/sample/annotation/Main.java
+++ src/main/java/org/mockcrumb/sample/annotation/Main.java
@@ -1,6 +1,7 @@
 package org.mockcrumb.sample.annotation;
 
 import org.mockcrumb.FileBasedMockcrumbLoader;
+import org.mockcrumb.MockcrumbLoaderRegistry;
 import org.mockcrumb.annotation.MockcrumbAnnotations;
 import org.mockcrumb.annotation.Mockcrumb;
 import org.mockcrumb.reader.JsonCrumbReader;
@@ -14,7 +15,7 @@ public final class Main {
     private Main() {
     }
 
-    public static final class FooEmbedded {
+    public static final class FooEmbedded1 {
         @Mockcrumb(name = "aaa")
         private Foo foo;
         @Mockcrumb
@@ -29,15 +30,52 @@ public final class Main {
         }
     }
 
+    public static final class FooEmbedded2 {
+        @Mockcrumb(name = "aaa")
+        private Foo foo;
+        @Mockcrumb
+        private Foo bbb;
+        @Mockcrumb(name = "aaa", loaderGroup = "another")
+        private Foo fooAnother;
+        @Mockcrumb(name = "bbb", loaderGroup = "another")
+        private Foo bbbAnother;
+
+        public Foo getFoo() {
+            return foo;
+        }
+
+        public Foo getBbb() {
+            return bbb;
+        }
+
+        public Foo getFooAnother() {
+            return fooAnother;
+        }
+
+        public Foo getBbbAnother() {
+            return bbbAnother;
+        }
+    }
+
     public static void main(final String[] args) {
         System.out.println("=== Started");
 
         Path contextPath = new File(Main.class.getClassLoader().getResource("sample-structure1").getPath()).toPath();
-        FooEmbedded fooEmbedded = new FooEmbedded();
-        MockcrumbAnnotations.init(fooEmbedded, FileBasedMockcrumbLoader.of(contextPath,
-                FullyQualifiedCrumbResolver.INSTANCE, JsonCrumbReader.INSTANCE));
-        System.out.println(fooEmbedded.getFoo().getValue());
-        System.out.println(fooEmbedded.getBbb().getValue());
+        FileBasedMockcrumbLoader loader = FileBasedMockcrumbLoader.of(contextPath,
+                FullyQualifiedCrumbResolver.INSTANCE, JsonCrumbReader.INSTANCE);
+
+        FooEmbedded1 fooEmbedded1 = new FooEmbedded1();
+        MockcrumbAnnotations.init(fooEmbedded1, loader);
+        System.out.println(fooEmbedded1.getFoo().getValue());
+        System.out.println(fooEmbedded1.getBbb().getValue());
+
+        FooEmbedded2 fooEmbedded2 = new FooEmbedded2();
+        MockcrumbAnnotations.init(fooEmbedded2,
+                MockcrumbLoaderRegistry.of(Mockcrumb.DEFAULT_LOADER_GROUP, loader).add("another", loader));
+        System.out.println(fooEmbedded2.getFoo().getValue());
+        System.out.println(fooEmbedded2.getBbb().getValue());
+        System.out.println(fooEmbedded2.getFooAnother().getValue());
+        System.out.println(fooEmbedded2.getBbbAnother().getValue());
 
         System.out.println("=== Finished");
     }
