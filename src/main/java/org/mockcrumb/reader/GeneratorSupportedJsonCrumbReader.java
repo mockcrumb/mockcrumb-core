f0984330d36a48e3300c2e1035bbf162acedd88c
Maciej Gowin <maciej.abacus.gowin@gmail.com>
2016-03-25 00:54:50 +0000

    Reuse file contents

--- src/main/java/org/mockcrumb/reader/GeneratorSupportedJsonCrumbReader.java
+++ src/main/java/org/mockcrumb/reader/GeneratorSupportedJsonCrumbReader.java
@@ -4,9 +4,9 @@ import com.google.gson.Gson;
 import org.mockcrumb.exception.MockcrumbException;
 import org.mockcrumb.generator.Generator;
 import org.mockcrumb.processor.CrumbContentProcessor;
+import org.mockcrumb.utils.FileContents;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -22,11 +22,7 @@ public class GeneratorSupportedJsonCrumbReader extends FileBasedCrumbReader {
     @Override
     public <T> T readContext(final Class<T> clazz, final Path context) {
         try {
-            StringBuilder builder = new StringBuilder();
-            for (String line : Files.readAllLines(context)) {
-                builder.append(line);
-            }
-            return GSON.fromJson(processor.process(builder.toString()), clazz);
+            return GSON.fromJson(processor.process(FileContents.get(context)), clazz);
         } catch (IOException ex) {
             throw new MockcrumbException(ex);
         }
--- src/main/java/org/mockcrumb/reader/JsonCrumbReader.java
+++ src/main/java/org/mockcrumb/reader/JsonCrumbReader.java
@@ -2,9 +2,9 @@ package org.mockcrumb.reader;
 
 import com.google.gson.Gson;
 import org.mockcrumb.exception.MockcrumbException;
+import org.mockcrumb.utils.FileContents;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class JsonCrumbReader extends FileBasedCrumbReader {
@@ -14,11 +14,7 @@ public class JsonCrumbReader extends FileBasedCrumbReader {
     @Override
     public <T> T readContext(final Class<T> clazz, final Path context) {
         try {
-            StringBuilder builder = new StringBuilder();
-            for (String line : Files.readAllLines(context)) {
-                builder.append(line);
-            }
-            return GSON.fromJson(builder.toString(), clazz);
+            return GSON.fromJson(FileContents.get(context), clazz);
         } catch (IOException ex) {
             throw new MockcrumbException(ex);
         }
